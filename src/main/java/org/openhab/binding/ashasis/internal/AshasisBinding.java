aba840a667e4f3aa0dc3b77b487acc749b7f6dc9
Ferry Israel <ferry.israel@wedekind.eu>
2015-06-11 12:19:42 +0200

    binding working now

--- src/main/java/org/openhab/binding/ashasis/internal/AshasisBinding.java
+++ src/main/java/org/openhab/binding/ashasis/internal/AshasisBinding.java
@@ -8,62 +8,119 @@
  */
 package org.openhab.binding.ashasis.internal;
 
+import java.io.InputStream;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
 import org.openhab.core.events.AbstractEventSubscriber;
 import org.openhab.core.items.Item;
 import org.openhab.core.types.Command;
 import org.openhab.core.types.State;
+import org.openhab.io.net.http.HttpUtil;
 import org.openhab.model.item.binding.BindingConfigParseException;
 import org.openhab.model.item.binding.BindingConfigReader;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-	
+
 
 /**
- * Implement this class if you are going create an actively polling service
- * like querying a Website/Device.
- * 
  * @author TF-Coding Webmedia
- * @since 1.0.0
+ * @since 1.8.0
  */
-public class AshasisBinding extends AbstractEventSubscriber implements BindingConfigReader {
+public class AshasisBinding extends AbstractEventSubscriber implements
+		BindingConfigReader, ManagedService {
 
-	private static final Logger logger = 		LoggerFactory.getLogger(AshasisBinding.class);
+	private String apiUrl = "";
+	
+	private boolean isNumeric(String str) {
+		try {
+			Double.parseDouble(str);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(AshasisBinding.class);
 
 	public AshasisBinding() {
 	}
-	
+
 	public String getBindingType() {
 		return "ashasis";
 	}
 
 	@Override
-	public void validateItemType(Item item, String bindingConfig) throws BindingConfigParseException {
-		
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+		logger.debug("called");
 	}
 
-	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException {
-		//	throw new BindingConfigParseException("WoL configuration must contain two parts (ip and macaddress separated by a '#'");
-			logger.debug("Binding to itm [{}]", item.getName());
+	public void activate(final BundleContext bundleContext, final Map<String, Object> configuration) throws ConfigurationException {
+		modified(configuration);
+	}
+		
+	public void modified(final Map<String, Object> configuration) throws ConfigurationException {
+		if(configuration!=null){
+			apiUrl = (String)configuration.get("apiurl");
+		}else{
+			throw new ConfigurationException("ashasis","Missing configuration: ashasis:apiurl");
+		}
+	}
+
+	@Override
+	public void validateItemType(Item item, String bindingConfig)
+			throws BindingConfigParseException {
+
+	}
+
+	HashMap<String, Item> registry = new HashMap<String, Item>();
+
+	public void processBindingConfiguration(String context, Item item,
+			String bindingConfig) throws BindingConfigParseException {
+		// throw new
+		// BindingConfigParseException("WoL configuration must contain two parts (ip and macaddress separated by a '#'");
+		registry.put(item.getName(), item);
 	}
 
 	public void removeConfigurations(String context) {
-		
+		registry.remove(context);
 	}
-	
+
 	@Override
 	public void receiveCommand(String itemName, Command command) {
-		// TODO Auto-generated method stub
-		this.relay(itemName, command.toString());
-		
+		if (isNumeric(command.toString())) {
+			// it's a ready to-use-value
+			this.relay(itemName, command.toString());
+
+		} else if (command.toString().contentEquals("TOGGLE")) {
+			// in future catch the commands here and react according
+			this.relay(itemName, command.toString());
+		}
+
 	}
 
 	@Override
 	public void receiveUpdate(String itemName, State newState) {
-		// TODO Auto-generated method stub
-		this.relay(itemName, newState.toString());
+		// this.relay(itemName, newState.toString());
 	}
 
-	private void relay(String itemName, String newVal){
+	private void relay(String itemName, String newVal) {
+
 		logger.debug("Got event [{}] to [{}]", itemName, newVal);
+		InputStream in = IOUtils.toInputStream("{\"item\":\"" + itemName
+				+ "\",\"value\":\"" + newVal + "\"}");
+		HttpUtil.executeUrl("POST", apiUrl + "/controller/push",
+				in, "application/json", 2500);
+		try {
+			in.close();
+		} catch (Exception e) {
+			// dont care about exception here
+		}
 	}
 }
